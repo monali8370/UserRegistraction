89b9052076ae04d5b2c9c81721eaaf780e061c95
monali8370 <monalishinde952@gmail.com>
2023-12-28 17:59:46 +0530

    created:user first name

---
+++ src/main/java/org/example/UserRegistrationProblem.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.util.regex.Pattern;
+public class UserRegistrationProblem {
+    public boolean firstNameValidation(String firstName) throws Exception {
+        if (Pattern.matches("[A-Z][a-z]{2,}", firstName))
+            return true;
+        else
+            throw new Exception("You enter Invalid First Name");
+        
+    }
+}
